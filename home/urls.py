4fc505ae3f0f2020394b4422e68443dc1c1e8f3c
sinanazem <sinanaazem@gmail.com>
2024-04-17 07:43:01 +0330

    update View http method

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, MongardView
 
 app_name='home'
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("mongard_view/", MongardView.as_view(), name="mongard_view"),
 ]
 
 
--- home/views.py
+++ home/views.py
@@ -5,3 +5,19 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         return render(request, 'home/home.html')
+
+
+
+class MongardView(View):
+    
+    def get(self, request):
+        return render(request, 'home/mongard_view.html')
+    
+    
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        response.headers['user'] = request.user
+        response.headers['user_authenticated_status'] = request.user.is_authenticated
+        return response
+    
+    
