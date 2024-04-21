148d5aaf933e146aff92090ce5cf4d75c1fd58ee
sinanazem <sinanaazem@gmail.com>
2024-04-21 17:17:02 +0330

    add LogoutView

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ class Car(models.Model):
     name = models.CharField(max_length=200)
     owner = models.CharField(max_length=200)
     year = models.PositiveBigIntegerField()
+    created = models.DateField(null=True, blank=True)
     
     def __str__(self):
         return f"{self.owner} - {self.name} - {self.year}"
